2b4954392ea4768aa1573d6cdebd8a6943f62e4c
kzar1102 <kzar1102@gmail.com>
2021-04-20 23:33:37 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# react-testing
+Express, React/Redux, MySQL
+
+### 1. Installation
+
+On the command prompt run the following commands:
+
+``` 
+ $ cp .env.example .env (edit it with your secret key and database information)
+ $ npm install
+ $ npm run migrate
+ ```
+ Finally, start and build the application:
+ 
+ ```
+ $ npm run build (For development)
+ $ npm run build:prod (For production)
+```
+
+### 2. Usage
+
+URL : http://localhost:3000/
+
---
+++ server/routes/user.route.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import * as userCtrl from '../controllers/user.controller';
+import isAuthenticated from '../middlewares/authenticate';
+import validate from '../config/joi.validate';
+import schema from '../utils/validator';
+
+const router = express.Router();
+
+router.route('/')
+    .post(validate(schema.storeUser), (req, res) => {
+        userCtrl.store(req, res);
+    })
+    .get( (req, res) => {
+        userCtrl.findAll(req, res);
+    });
+
+
+router.route('/:id')
+    .get( (req, res) => {
+        userCtrl.findById(req, res);
+    })
+    .put(isAuthenticated, (req, res) => {
+        userCtrl.update(req, res);
+    })
+    .delete(isAuthenticated, (req, res) => {
+        userCtrl.destroy(req, res);
+    });
+
+
+export default router;
